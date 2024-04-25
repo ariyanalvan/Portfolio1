a19ae3737dc70fb2c76b83a4693d1fdfde81cfa7
Ariyan islam <68250910+ariyanalvan@users.noreply.github.com>
2024-04-25 19:04:43 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        title:["Poppins","sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
